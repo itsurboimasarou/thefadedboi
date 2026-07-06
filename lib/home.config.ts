348d2c662d35e69c6e7c058906ad89a274c6ef81
itsurboimasarou <itsurboimasarou@gmail.com>
2026-07-06 19:51:03 +0700

    home: Hey Furina why are you here?

--- lib/home.config.ts
+++ lib/home.config.ts
@@ -1,7 +1,7 @@
 import type { Status } from "./types";
 
 export const home = {
-  eyebrow: "Welcome to my corner of the web (err, Furina I guess...?)",
+  eyebrow: "Welcome! ...Furina?",
   quickShorts:
     "A lowkey guy made a lowkey place for anything you want to know about me, from my photoworks, cosplays, to my side projects. I do random things, even reviews",
   status: { state: "online", label: "Online" } as Status,
