66e1f2d8740ce67b142cb988957368becdf856a8
itsurboimasarou <itsurboimasarou@gmail.com>
2026-07-07 14:50:09 +0700

    css: Tune light mode box background for Lite mode

--- lib/site.config.ts
+++ lib/site.config.ts
@@ -2,6 +2,6 @@ export const site = {
   name: "thefadedboi",
   handle: "thefadedboi",
   role: "Part time designer-developer hybrid, photophoner, cosplayer in your area",
-  tagline: "I do random things, fell free to reach out to me if you want a collab to anything (just not for any kind of NSFW contents).",
+  tagline: "I do random things, feel free to reach out to me if you want a collab to anything (just not for any kind of NSFW contents).",
   footer: "Copyright © 2026 by thefadedboi - Hosted with 🧡 on Cloudflare.",
 };
