fdda53e603be28746e01b4e1e670ade891a25136
itsurboimasarou <itsurboimasarou@gmail.com>
2026-07-07 23:18:16 +0700

    overall: Bring-up final touches & ready for launch

--- lib/site.config.ts
+++ lib/site.config.ts
@@ -3,5 +3,5 @@ export const site = {
   handle: "thefadedboi",
   role: "Part time designer-developer hybrid, photophoner, cosplayer in your area",
   tagline: "I do random things, feel free to reach out to me if you want a collab to anything (just not for any kind of NSFW contents).",
-  footer: "Copyright © 2026 by thefadedboi - Hosted with 🧡 on Cloudflare.",
+  footer: "Copyright © 2026 by masarou/thefadedboi - Furina images belongs to COGNOSPHERE PTE. LTD. All rights reserved.",
 };
